506802b9610a6152e53934ba8ab4d22a2a6a65cb
fahadsaeed8 <120630793+fahadsaeed8@users.noreply.github.com>
2025-06-12 11:28:16 +0500

    mobile responsive

--- components/Herosection.tsx
+++ components/Herosection.tsx
@@ -58,7 +58,7 @@ const Herosection = () => {
     <div className="relative min-h-screen mt-15 flex items-start justify-start px-12 bg-transparent">
       <div className="z-10 max-w-4xl w-full">
         {/* Slider content */}
-        <div className="relative px-[60px] flex flex-col items-start gap-4 transition-all duration-500 ease-in-out">
+        <div className="relative flex md:px-[60px]  flex-col items-start gap-4 transition-all duration-500 ease-in-out">
           {/* Logo */}
           <Image
             src="/PROSIX-LOGO.png"
@@ -76,10 +76,10 @@ const Herosection = () => {
                 : "opacity-100 translate-y-0"
             }`}
           >
-            <p className="text-[25px] font-bold text-gray-300 tracking-[3px]">
+            <p className="text-[21px] md:text-[25px] font-bold text-gray-300 tracking-[3px]">
               {messages[currentIndex].text}
             </p>
-            <p className="text-[46px] w-[65%] tracking-normal text-white ">
+            <p className="text-[38px] md:text-[46px] w-[65%] tracking-normal text-white ">
               {messages[currentIndex].subText}
             </p>
           </div>
@@ -91,7 +91,7 @@ const Herosection = () => {
         </div>
 
         {/* Navigation buttons */}
-        <div className="absolute top-[100px] -translate-y-1/2 left-4 sm:left-8 z-20">
+        <div className="absolute md:block hidden top-[100px] -translate-y-1/2 left-4 sm:left-8 z-20">
           <button
             onClick={handlePrev}
             className="bg-gray-200 text-black cursor-pointer hover:bg-gray-400 px-[12px] py-[6px] rounded-full shadow transition"
@@ -99,7 +99,7 @@ const Herosection = () => {
             <i className="fas fa-chevron-left" />
           </button>
         </div>
-        <div className="absolute top-[100px] -translate-y-1/2 right-4 sm:right-8 z-20">
+        <div className="absolute md:block hidden top-[100px] -translate-y-1/2 right-4 sm:right-8 z-20">
           <button
             onClick={handleNext}
             className="bg-gray-200 text-black cursor-pointer hover:bg-gray-400 px-[12px] py-[6px] rounded-full shadow transition"
