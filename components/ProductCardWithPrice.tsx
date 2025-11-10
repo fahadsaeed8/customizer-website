5821114cebca654066527520cd51dfc8bc383a00
MayerKhawaja <112714767+MayerKhawaja@users.noreply.github.com>
2025-11-10 21:40:58 +0500

    changed the width of product card with price and changed there hover color

--- components/ProductCardWithPrice.tsx
+++ components/ProductCardWithPrice.tsx
@@ -18,16 +18,16 @@ const ProductCardWithPrice: React.FC<ProductCardWithProps> = ({
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="border border-gray-300 p-3 rounded text-center shadow-xl hover:shadow-lg transition">
+    <div className="border border-gray-300 p-3 rounded text-center shadow-xl hover:shadow-lg transition w-62">
       <img
         src={imageSrc}
         alt={`Product Image of ${name}`}
-        className="w-full h-48 object-contain mx-auto"
+        className="w-full h-56 object-contain mx-auto"
       />
 
       <h3 className="font-semibold uppercase mt-2 text-2xl">{name}</h3>
 
-      <div className="flex justify-center  text-[30px] font-bold mb-1 space-x-1">
+      <div className="flex justify-center text-[30px] font-bold mb-1 space-x-1">
         <span className="text-lg font-semibold !align-top">$</span>
         <span className="sm:text-4xl text-4xl font-semibold">{price}.</span>
         <span className="text-lg font-semibold align-top">00</span>
@@ -37,12 +37,12 @@ const ProductCardWithPrice: React.FC<ProductCardWithProps> = ({
         {link ? (
           <Link
             href={link}
-            className="bg-[#ad2525] text-white text-[19px] cursor-pointer px-4 py-1 rounded-md hover:bg-[#a2e632] transition mt-2 block text-center"
+            className="bg-[#ad2525] text-white text-[19px] cursor-pointer px-4 py-1 rounded-md hover:bg-[#414141] transition mt-2 block text-center"
           >
             Select options
           </Link>
         ) : (
-          <button className="bg-[#ad2525] text-white text-[19px] cursor-pointer px-4 py-1 rounded-md hover:bg-[#a2e632] transition mt-2">
+          <button className="bg-[#ad2525] text-white text-[19px] cursor-pointer px-4 py-1 rounded-md hover:bg-[#414141] transition mt-2">
             Select options
           </button>
         )}
